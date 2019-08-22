65f2793c0a530877cd1bf540451de7c755789018
Joe <josephc.lambert@gmail.com>
2019-08-22 13:18:52 -0700

    refactors Villain and Hero to class syntax

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -197,19 +197,33 @@ console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
 
-function Villain(attributes) {
-  Humanoid.call(this, attributes);
-  this.isVillain = attributes.isVillain;
+// function Villain(attributes) {
+//   Humanoid.call(this, attributes);
+//   this.isVillain = attributes.isVillain;
+// }
+
+// Villain.prototype = Object.create(Humanoid.prototype);
+
+class Villain extends Humanoid {
+  constructor(attr) {
+    super(attr);
+    this.isVillain = attr.isVillain;
+  }
 }
 
-Villain.prototype = Object.create(Humanoid.prototype);
+// function Hero(attributes) {
+//   Humanoid.call(this, attributes);
+//   this.isHero = attributes.isHero;
+// }
 
-function Hero(attributes) {
-  Humanoid.call(this, attributes);
-  this.isHero = attributes.isHero;
+// Hero.prototype = Object.create(Humanoid.prototype);
+
+class Hero extends Humanoid {
+  constructor(attr) {
+    super(attr);
+    this.isHero = attr.isHero;
+  }
 }
-
-Hero.prototype = Object.create(Humanoid.prototype);
 
 const baddie = new Villain({
   createdAt: new Date(),
